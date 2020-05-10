ee387174c908c76d25385b363c791084e5b05c1a
WadeeaKiwan <wadeea.kiwan@hotmail.com>
2020-05-10 17:24:27 +0200

    Starting Setup, Pages & Routes

---
+++ frontend/src/places/pages/NewPlace.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NewPlace = (): JSX.Element => {
+  return <h2>NewPlace Works!</h2>;
+};
+
+export default NewPlace;
---
+++ frontend/src/user/pages/Users.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Users = (): JSX.Element => {
+  return <h2>Users Works!</h2>;
+};
+
+export default Users;
