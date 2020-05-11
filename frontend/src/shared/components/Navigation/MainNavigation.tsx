ef50f62a518c9909aa85ea177e84c273fe59d8e6
WadeeaKiwan <wadeea.kiwan@hotmail.com>
2020-05-11 20:39:57 +0200

    Adding Navlinks

--- frontend/src/shared/components/Navigation/MainNavigation.tsx
+++ frontend/src/shared/components/Navigation/MainNavigation.tsx
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import "./MainNavigation.css";
 
 import MainHeader from "./MainHeader";
+import NavLinks from "./NavLinks";
 
 const MainNavigation: React.FC = (props) => {
   return (
@@ -16,7 +17,9 @@ const MainNavigation: React.FC = (props) => {
       <h1 className='main-navigation__title'>
         <Link to='/'>OurPlaces</Link>
       </h1>
-      <nav>...</nav>
+      <nav>
+        <NavLinks />
+      </nav>
     </MainHeader>
   );
 };
