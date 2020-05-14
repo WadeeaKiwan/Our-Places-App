57d1718bb183045a70a9831eb6b89651b1d39156
WadeeaKiwan <wadeea.kiwan@hotmail.com>
2020-05-14 11:31:39 +0200

    Adding Custom Buttons

--- frontend/src/places/components/PlaceItem.tsx
+++ frontend/src/places/components/PlaceItem.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./PlaceItem.css";
 
 import Card from "../../shared/components/UIElements/Card";
+import Button from "../../shared/components/FormElements/Button";
 
 type Props = Readonly<{
   place: Place;
@@ -22,9 +23,9 @@ const PlaceItem: React.FC<Props> = ({
           <p>{description}</p>
         </div>
         <div className='place-item__actions'>
-          <button>VIEW ON MAP</button>
-          <button>EDIT</button>
-          <button>DELETE</button>
+          <Button inverse>VIEW ON MAP</Button>
+          <Button to={`/places/${id}`}>EDIT</Button>
+          <Button danger>DELETE</Button>
         </div>
       </Card>
     </li>
