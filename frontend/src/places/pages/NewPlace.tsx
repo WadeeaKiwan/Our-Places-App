e8d94a81aeba3dd02facd9294d39fbebb7d488f5
WadeeaKiwan <wadeea.kiwan@hotmail.com>
2020-05-14 18:03:00 +0200

    Managing Form-wide State

--- frontend/src/places/pages/NewPlace.tsx
+++ frontend/src/places/pages/NewPlace.tsx
@@ -1,13 +1,66 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useReducer } from "react";
 import "./NewPlace.css";
 
 import Input from "../../shared/components/FormElements/Input";
+import Button from "../../shared/components/FormElements/Button";
 import { VALIDATOR_REQUIRE, VALIDATOR_MINLENGTH } from "../../shared/util/validators";
 
-const NewPlace: React.FC = () => {
-  const titleInputHandler = useCallback((id, value, isValid) => {}, []);
+type FormReducerState = {
+  inputs: {
+    [x: string]: { value: string; isValid: boolean };
+  };
+  isValid: boolean;
+};
 
-  const descriptionInputHandler = useCallback((id, value, isValid) => {}, []);
+type FormReducerAction = {
+  type: string;
+  inputId: string;
+  isValid: boolean;
+  value: string;
+};
+
+const formReducer = (state: FormReducerState, action: FormReducerAction) => {
+  switch (action.type) {
+    case "INPUT_CHANGE":
+      let formIsValid = true;
+      for (const inputId in state.inputs) {
+        if (inputId === action.inputId) {
+          formIsValid = formIsValid && action.isValid;
+        } else {
+          formIsValid = formIsValid && state.inputs[inputId].isValid;
+        }
+      }
+      return {
+        ...state,
+        inputs: {
+          ...state.inputs,
+          [action.inputId]: { value: action.value, isValid: action.isValid }
+        },
+        isValid: formIsValid
+      };
+    default:
+      return state;
+  }
+};
+
+const NewPlace: React.FC = () => {
+  const [formState, dispatch] = useReducer(formReducer, {
+    inputs: {
+      title: {
+        value: "",
+        isValid: false
+      },
+      description: {
+        value: "",
+        isValid: false
+      }
+    },
+    isValid: false
+  });
+
+  const inputHandler = useCallback((id, value, isValid) => {
+    dispatch({ type: "INPUT_CHANGE", value, isValid, inputId: id });
+  }, []);
 
   return (
     <form className='place-form'>
@@ -18,7 +71,7 @@ const NewPlace: React.FC = () => {
         label='Title'
         validators={[VALIDATOR_REQUIRE()]}
         errorText='Please enter a valid title.'
-        onInput={titleInputHandler}
+        onInput={inputHandler}
       />
       <Input
         id='description'
@@ -26,8 +79,11 @@ const NewPlace: React.FC = () => {
         label='Description'
         validators={[VALIDATOR_MINLENGTH(5)]}
         errorText='Please enter a valid description (at least 5 characters).'
-        onInput={descriptionInputHandler}
+        onInput={inputHandler}
       />
+      <Button type='submit' disabled={!formState.isValid}>
+        ADD PLACE
+      </Button>
     </form>
   );
 };
