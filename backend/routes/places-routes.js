6805a734d8be1927bbcf34dbdbd636b8b16ed764
WadeeaKiwan <wadeea.kiwan@hotmail.com>
2020-05-19 17:41:24 +0200

    Adding Place-Specific Routes

--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -2,9 +2,52 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  console.log("GET Request in Places");
-  res.json({ message: "It works!" });
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    imageUrl:
+      "https://firebasestorage.googleapis.com/v0/b/socialape-ad195.appspot.com/o/matthias-schroder-KoBCaTPydqs-unsplash.jpg?alt=media",
+    title: "Strada Statale Porrettana",
+    description: "Strada Statale Porrettana",
+    address: "Strada Statale Porrettana, 240, 40135 Bologna BO, Italy, Bologna",
+    creatorId: "u1",
+    location: {
+      lat: 44.4896115,
+      lng: 11.3129859
+    }
+  },
+  {
+    id: "p2",
+    imageUrl:
+      "https://firebasestorage.googleapis.com/v0/b/socialape-ad195.appspot.com/o/filip-mroz-VH7NuUbj104-unsplash.jpg?alt=media",
+    title: "One Queen Street East",
+    description: "One Queen Street East",
+    address: "One Queen Street East, Toronto, Canada",
+    creatorId: "u1",
+    location: {
+      lat: 43.6672424,
+      lng: -79.395718
+    }
+  },
+  {
+    id: "p3",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/7/7c/Empire_State_Building%2C_New_York%2C_NY.jpg",
+    title: "Empire State Building",
+    description: "One of the most sky scrapers in the world!",
+    address: "20 W 34th St, New York, NY 10001, United States",
+    creatorId: "u2",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9856644
+    }
+  }
+];
+
+router.get("/:pid", (req, res, next) => {
+  const placeId = req.params.pid; // { pid: 'p1' }
+  const place = DUMMY_PLACES.find((p) => p.id === placeId);
+  res.json({ place }); // => { place } => { place: place }
 });
 
 module.exports = router;
