5859e57332db62d62cdb9b205e0ddce26d82cb91
WadeeaKiwan <wadeea.kiwan@hotmail.com>
2020-05-20 05:23:17 +0200

    Adding PATCH Route to Update Places & DELETE Place Route

--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -1,11 +1,19 @@
 const express = require("express");
 
-const { getPlaceById, getPlaceByUserId, createPlace } = require("../controllers/places-controller");
+const {
+  getPlaceById,
+  getPlaceByUserId,
+  createPlace,
+  updatePlace,
+  deletePlace
+} = require("../controllers/places-controller");
 
 const router = express.Router();
 
 router.get("/:pid", getPlaceById);
 router.get("/user/:uid", getPlaceByUserId);
 router.post("/", createPlace);
+router.patch("/:pid", updatePlace);
+router.delete("/:pid", deletePlace);
 
 module.exports = router;
