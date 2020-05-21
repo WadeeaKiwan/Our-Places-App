3bd0cdb412a2cc6efadf6583ca89d5f866b683a1
WadeeaKiwan <wadeea.kiwan@hotmail.com>
2020-05-21 16:27:21 +0200

    Adding the User Login

--- backend/app.js
+++ backend/app.js
@@ -30,7 +30,8 @@ app.use((error, req, res, next) => {
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useCreateIndex: true
   })
   .then(() => {
     console.log("Connected to the database!");
