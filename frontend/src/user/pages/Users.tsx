9cec98afd5feab329c0a783a987d8793a5ac234c
WadeeaKiwan <wadeea.kiwan@hotmail.com>
2020-05-27 16:39:26 +0200

    Getting Users with a GET Request

--- frontend/src/user/pages/Users.tsx
+++ frontend/src/user/pages/Users.tsx
@@ -1,26 +1,67 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import UsersList from "../components/UsersList";
+import ErrorModal from "../../shared/components/UIElements/ErrorModal";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
 
-const DUMMY_USERS: User[] = [
-  {
-    id: "u1",
-    name: "Wadeea Kiwan",
-    image:
-      "https://firebasestorage.googleapis.com/v0/b/socialape-ad195.appspot.com/o/995518179.jpg?alt=media",
-    placeCount: 2
-  },
-  {
-    id: "u2",
-    name: "Max Bla",
-    image:
-      "https://firebasestorage.googleapis.com/v0/b/socialape-ad195.appspot.com/o/480532018.jpg?alt=media",
-    placeCount: 1
-  }
-];
+// const DUMMY_USERS: User[] = [
+//   {
+//     id: "u1",
+//     name: "Wadeea Kiwan",
+//     image:
+//       "https://firebasestorage.googleapis.com/v0/b/socialape-ad195.appspot.com/o/995518179.jpg?alt=media",
+//     placeCount: 2
+//   },
+//   {
+//     id: "u2",
+//     name: "Max Bla",
+//     image:
+//       "https://firebasestorage.googleapis.com/v0/b/socialape-ad195.appspot.com/o/480532018.jpg?alt=media",
+//     placeCount: 1
+//   }
+// ];
 
 const Users: React.FC = () => {
-  return <UsersList users={DUMMY_USERS} />;
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [loadedUsers, setLoadedUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    const sedRequest = async () => {
+      setIsLoading(true);
+      try {
+        const response = await fetch("http://localhost:5000/api/users");
+
+        const responseData = await response.json();
+
+        if (!response.ok) {
+          throw new Error(responseData.message);
+        }
+
+        setLoadedUsers(responseData.users);
+      } catch (err) {
+        setError(err.message);
+      }
+      setIsLoading(false);
+    };
+    sedRequest();
+  }, []);
+
+  const errorHandler = () => {
+    setError(null);
+  };
+
+  return (
+    <React.Fragment>
+      <ErrorModal error={error} onClear={errorHandler} />
+      {isLoading && (
+        <div className='center'>
+          <LoadingSpinner />
+        </div>
+      )}
+      {!isLoading && loadedUsers && <UsersList users={loadedUsers} />};
+    </React.Fragment>
+  );
 };
 
 export default Users;
