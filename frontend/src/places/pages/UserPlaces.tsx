aa1bb060a0abc9c7fd10b084aec74fff4940f090
WadeeaKiwan <wadeea.kiwan@hotmail.com>
2020-05-27 23:37:47 +0200

    Loading Places by User Id

--- frontend/src/places/pages/UserPlaces.tsx
+++ frontend/src/places/pages/UserPlaces.tsx
@@ -1,61 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { useHttpClient } from "../../shared/hooks/http-hook";
 
 import PlaceList from "../components/PlaceList";
-
-const DUMMY_PLACES: Place[] = [
-  {
-    id: "p1",
-    imageUrl:
-      "https://firebasestorage.googleapis.com/v0/b/socialape-ad195.appspot.com/o/matthias-schroder-KoBCaTPydqs-unsplash.jpg?alt=media",
-    title: "Strada Statale Porrettana",
-    description: "Strada Statale Porrettana",
-    address: "Strada Statale Porrettana, 240, 40135 Bologna BO, Italy, Bologna",
-    creatorId: "u1",
-    location: {
-      lat: 44.4896115,
-      lng: 11.3129859
-    }
-  },
-  {
-    id: "p2",
-    imageUrl:
-      "https://firebasestorage.googleapis.com/v0/b/socialape-ad195.appspot.com/o/filip-mroz-VH7NuUbj104-unsplash.jpg?alt=media",
-    title: "One Queen Street East",
-    description: "One Queen Street East",
-    address: "One Queen Street East, Toronto, Canada",
-    creatorId: "u1",
-    location: {
-      lat: 43.6672424,
-      lng: -79.395718
-    }
-  },
-  {
-    id: "p3",
-    imageUrl:
-      "https://upload.wikimedia.org/wikipedia/commons/7/7c/Empire_State_Building%2C_New_York%2C_NY.jpg",
-    title: "Empire State Building",
-    description: "One of the most sky scrapers in the world!",
-    address: "20 W 34th St, New York, NY 10001, United States",
-    creatorId: "u2",
-    location: {
-      lat: 40.7484405,
-      lng: -73.9856644
-    }
-  }
-];
+import ErrorModal from "../../shared/components/UIElements/ErrorModal";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
 
 type ParamTypes = Readonly<{
   userId: string;
 }>;
 
 const UserPlaces: React.FC = () => {
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
+  const [loadedPlaces, setLoadedPlaces] = useState<Place[]>([]);
   const { userId } = useParams<ParamTypes>();
-  const loadedPlaces = DUMMY_PLACES.filter((place) => place.creatorId === userId);
+
+  useEffect(() => {
+    const fetchPlaces = async () => {
+      try {
+        const responseData = await sendRequest(`http://localhost:5000/api/places/user/${userId}`);
+
+        setLoadedPlaces(responseData.places);
+      } catch (err) {}
+    };
+    fetchPlaces();
+  }, [sendRequest, userId]);
 
   return (
     <React.Fragment>
-      <PlaceList places={loadedPlaces} />
+      <ErrorModal error={error} onClear={clearError} />
+      {isLoading && (
+        <div className='center'>
+          <LoadingSpinner isOverlay />}
+        </div>
+      )}
+      {!isLoading && loadedPlaces && <PlaceList places={loadedPlaces} />}
     </React.Fragment>
   );
 };
