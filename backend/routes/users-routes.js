73daa8fb189754918c8f96d840aaf5384c703c8c
WadeeaKiwan <wadeea.kiwan@hotmail.com>
2020-05-30 07:27:06 +0200

    Check that image exists and the mimetype backend

--- backend/routes/users-routes.js
+++ backend/routes/users-routes.js
@@ -3,13 +3,10 @@ const { check } = require("express-validator");
 
 const { getUsers, signup, login } = require("../controllers/users-controller");
 const fileUpload = require("../middleware/file-upload");
-const checkAuth = require("../middleware/check-auth");
 
 const router = express.Router();
 
 router.get("/", getUsers);
-
-// router.use(checkAuth);
 
 router.post(
   "/signup",
@@ -17,7 +14,8 @@ router.post(
   [
     check("name").not().isEmpty(),
     check("email").normalizeEmail().isEmail(), // normalizeEmail() => Test@test.com => test@test.com
-    check("password").isLength({ min: 6 })
+    check("password").isLength({ min: 6 }),
+    check("image").isMimeType()
   ],
   signup
 );
